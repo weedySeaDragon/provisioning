644d27996c6dcf70c224d5fe227043f660cd7d9e
Andrew Spinks <aspinks@dius.com.au>
2012-11-10 21:07:36 +1100

    Modify nginx config to use ssl. Only supports self signed cert at the moment.

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -9,6 +9,11 @@ namespace :nginx do
 
   desc "Setup nginx configuration for this application"
   task :setup, roles: :web do
+    puts "TODO: Support supplied SSL certificate!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    nginx_conf = "/etc/nginx"
+    run "#{sudo} openssl req -new -nodes -keyout #{nginx_conf}/server.key -out #{nginx_conf}/server.csr -subj \"/C=AU/ST=Victoria/L=Melbourne/O=#{application}/OU=IT/CN=#{application}\""
+    run "#{sudo} openssl x509 -req -days 365 -in #{nginx_conf}/server.csr -signkey #{nginx_conf}/server.key -out #{nginx_conf}/server.crt"
+
     template "nginx_unicorn.erb", "/tmp/nginx_conf"
     run "#{sudo} mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
     run "#{sudo} rm -f /etc/nginx/sites-enabled/default"
