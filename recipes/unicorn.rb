542b2f41eafa9db5f5755f17ec6097eb2576c369
Andrew Spinks <aspinks@dius.com.au>
2013-07-25 14:26:29 +1000

    Add setup of git post-receive deploy hook.

--- recipes/unicorn.rb
+++ recipes/unicorn.rb
@@ -2,10 +2,12 @@ set_default(:unicorn_user) { app_user }
 set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
 set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
 set_default(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
+set_default(:git_post_receive) { "#{current_path}/.git/hooks/post-receive" }
 set_default(:unicorn_workers, 2)
 
 after "provision:install", "unicorn:install"
 after "deploy:setup", "unicorn:setup"
+after "deploy", "unicorn:add_git_hook"
 
 namespace :unicorn do
   desc "Setup Unicorn initializer and app configuration"
@@ -19,6 +21,11 @@ namespace :unicorn do
   task :setup, roles: :app do
     run "mkdir -p #{shared_path}/config"
     template "unicorn.rb.erb", unicorn_config
+  end
+
+  task :add_git_hook do
+    template "post-receive.erb", git_post_receive
+    run "chmod a+x #{git_post_receive}"
   end
 
   %w[start stop restart].each do |command|
